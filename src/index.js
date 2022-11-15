9c83e8a3a0a05ae161d07a2e995980939407c225
HiImZsofi <zsofia.gyori00@gmail.com>
2022-11-15 09:11:18 +0100

    masodikat nem tudom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css';
 let qlist = []
+let thelist = []
 
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById("all").addEventListener('click', async () => {
@@ -17,11 +18,41 @@ async function all(){
 
     for (let p of result.quotes) {
         let li = document.createElement('li')
-        console.log(p)
         li.textContent = p.author + " : '" + p.quote + "'"
-        document.getElementById("quoteslist").appendChild(li);
+        document.getElementById("quoteslist").appendChild(li)
         qlist.push(result.quote)
         qlist.push(result.author)
     }
 }
+
+async function the(){
+    let response = await fetch('quotes.json')
+    let result = await response.json()
+
+    for (let p of result.quotes) {
+        thelist.push(p.quote)
+
+        // thelist.textContent = ''
+
+        // if(thelist.filter(word => word.length > 6)){
+
+        // }
+        // let li = document.createElement('li')
+        // li.textContent = p.quote
+        // document.getElementById("thelist").appendChild(li)
+    }
+
+    let wordArray = thelist.split(' ')
+    let word = "the"
+
+    for (let i of wordArray) {
+        thelist.textContent = ''
+        if(wordArray.filter(sw => word.includes(sw.toLowerCase()))){
+            //HOGY?????????????
+        }
+
+    }
+
+}
+
 
